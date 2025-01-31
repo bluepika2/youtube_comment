7d6eaf8100b0264aec541926b9bfc87b6a905b7a
Sanghoon Yoo <ericyoo@umich.edu>
2025-01-30 21:03:49 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from run import run
+
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    run()
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
